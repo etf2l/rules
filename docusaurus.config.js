3bf5796c5e3d85f0e1b54a99f97c4919af976152
Radosław Serba <radoslaw@serba.ovh>
2024-04-27 18:32:36 +0200

    fix: edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/etf2l/rules',
+            'https://github.com/etf2l/rules/edit/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
